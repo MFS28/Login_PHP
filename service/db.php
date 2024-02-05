e843d08687f67ed8fa04e723707412b2fc543e65
MFS28 <158980222+MFS28@users.noreply.github.com>
2024-02-05 09:25:50 +0700

    Mengupload file koneksi database'

---
+++ service/db.php
@@ -0,0 +1,8 @@
+<?php
+  $hostname = "127.0.0.1";
+  $username = "root";
+  $password = "";
+  $database_name = "buku_tamu";
+  
+  $db = mysqli_connect($hostname,$username,$password,$database_name);
+?>
